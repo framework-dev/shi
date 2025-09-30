4172515b54630111aff6190420b6294f4a6080b5
John Cayley <John_Cayley@brown.edu>
2025-09-30 16:44:29 -0400

    now using framework built in Source Serif 4

--- observablehq.config.js
+++ observablehq.config.js
@@ -17,10 +17,10 @@ export default {
   // ],
 
   // Content to add to the head of the page, e.g. for a favicon:
-  head: `<link rel="icon" href="favicon.ico" type="image/x-icon" sizes="32x32">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Source+Serif+4:ital,opsz,wght@0,8..60,200..900;1,8..60,200..900&display=swap" rel="stylesheet"></link>`,
+  head: `<link rel="icon" href="favicon.ico" type="image/x-icon" sizes="32x32">`,
+  // <link rel="preconnect" href="https://fonts.googleapis.com">
+  // <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  // <link href="https://fonts.googleapis.com/css2?family=Source+Serif+4:ital,opsz,wght@0,8..60,200..900;1,8..60,200..900&display=swap" rel="stylesheet"></link>`,
 
   // The path to the source root.
   root: "src",
